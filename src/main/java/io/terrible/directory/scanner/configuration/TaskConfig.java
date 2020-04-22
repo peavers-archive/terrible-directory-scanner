7ff6ed4774a1b69d2ad92029498ce612cb75f431
Chris Turner <chris@forloop.space>
2020-04-22 09:29:12 +0100

    fix: Forcefully exit when done

--- src/main/java/io/terrible/directory/scanner/configuration/TaskConfig.java
+++ src/main/java/io/terrible/directory/scanner/configuration/TaskConfig.java
@@ -4,12 +4,13 @@ package io.terrible.directory.scanner.configuration;
 import com.beust.jcommander.JCommander;
 import io.terrible.directory.scanner.Application;
 import io.terrible.directory.scanner.service.ScanService;
-import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @Configuration
@@ -33,6 +34,8 @@ public class TaskConfig {
       JCommander.newBuilder().addObject(this.args).build().parse(args);
 
       scanService.scanMedia(this.args.getDirectory());
+
+      System.exit(0);
     };
   }
 }
